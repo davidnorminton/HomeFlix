35bb95b654a2379fca29c5bd7a33a2530024173e
David Norminton <david.norminton@gmail.com>
2024-08-30 21:46:41 +0100

    Setup react with electron

---
+++ electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+  });
+
+  // Always load the index.html file from the build directory
+  const startURL = `file://${path.join(__dirname, 'build', 'index.html')}`;
+
+  mainWindow.loadURL(startURL);
+
+  // Optionally, open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
